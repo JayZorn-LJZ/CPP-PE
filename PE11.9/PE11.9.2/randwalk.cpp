8980072c1eff6743b2c729275ee03f86de31d796
Phil-LJZ <a13926719253@163.com>
2025-03-22 11:28:32 +0800

    None

---
+++ PE11.9/PE11.9.2/randwalk.cpp
@@ -0,0 +1,111 @@
+// // randwalk.cpp -- using the Vector class
+// // compile with the vect.cpp file
+// #include <iostream>
+// #include <cstdlib>      // rand(), srand() prototypes
+// #include <ctime>        // time() prototype
+// #include "vect.h"
+// int main()
+// {
+//     using namespace std;
+//     using VECTOR::Vector;
+//     srand(time(0));     // seed random-number generator
+//     double direction;
+//     Vector step;
+//     Vector result(0.0, 0.0);
+//     unsigned long steps = 0;
+//     double target;
+//     double dstep;
+//     cout << "Enter target distance (q to quit): ";
+//     while (cin >> target)
+//     {
+//         cout << "Enter step length: ";
+//         if (!(cin >> dstep))
+//             break;
+
+//         while (result.magval() < target)
+//         {
+//             direction = rand() % 360;
+//             step.reset(dstep, direction, Vector::POL);
+//             result = result + step;
+//             steps++;
+//         }
+//         cout << "After " << steps << " steps, the subject "
+//             "has the following location:\n";
+//         cout << result << endl;
+//         result.polar_mode();
+//         cout << " or\n" << result << endl;
+//         cout << "Average outward distance per step = "
+//             << result.magval()/steps << endl;
+//         steps = 0;
+//         result.reset(0.0, 0.0);
+//         cout << "Enter target distance (q to quit): ";
+//     }
+//     cout << "Bye!\n";
+// /* keep window open
+//     cin.clear();
+//     while (cin.get() != '\n')
+//         continue;
+//     cin.get();
+// */
+//     return 0; 
+// }
+
+// randwalk.cpp -- using the Vector class
+// compile with the vect.cpp file
+#include <iostream>
+#include <cstdlib>      // rand(), srand() prototypes
+#include <ctime>        // time() prototype
+#include <fstream>
+#include "vect.h"
+int main()
+{
+    using namespace std;
+    using VECTOR::Vector;
+    srand(time(0));     // seed random-number generator
+    double direction;
+    Vector step;
+    Vector result(0.0, 0.0);
+    unsigned long steps = 0;
+    double target;
+    double dstep;
+    ofstream outfile; // 文件对象
+
+    cout << "Enter target distance (q to quit): ";
+    while (cin >> target)
+    {
+        outfile.open("test.txt"); // 重新打开以实现覆写
+        cout << "Enter step length: ";
+        if (!(cin >> dstep))
+            break;
+
+        outfile << "Target Distance: " << target << ", Step Size: " << dstep << endl;
+        outfile << "0: (x,y) = (0, 0)" << endl; // 输出第零步
+        while (result.magval() < target)
+        {
+            direction = rand() % 360;
+            step.reset(dstep, direction, Vector::POL);
+            result = result + step;
+            outfile << steps + 1 << ": " << result << endl; // 输出具体步信息
+            steps++;
+        }
+        outfile << "After " << steps << " steps, the subject "
+            "has the following location:\n";
+        outfile << result << endl;
+        result.polar_mode();
+        outfile << " or\n" << result << endl;
+        outfile << "Average outward distance per step = "
+            << result.magval()/steps << endl;
+        steps = 0;
+        result.reset(0.0, 0.0);
+        cout << "Enter target distance (q to quit): ";
+        outfile.close();
+    }
+    cout << "Bye!\n";
+/* keep window open
+    cin.clear();
+    while (cin.get() != '\n')
+        continue;
+    cin.get();
+*/
+    return 0; 
+}
